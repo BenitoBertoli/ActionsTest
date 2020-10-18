fa6e42caa3f820b64e4d526d248f88a18fa0fd85
Benito Bertoli <binoprod@gmail.com>
2020-10-19 00:36:32 +0300

    again

--- app/src/test/java/com/benitobertoli/actionstest/UtilsTest.kt
+++ app/src/test/java/com/benitobertoli/actionstest/UtilsTest.kt
@@ -7,6 +7,6 @@ class UtilsTest {
 
     @Test
     fun `sum should return the correct sum`() {
-        Assert.assertEquals(4, Utils.sum(1, 2))
+        Assert.assertEquals(5, Utils.sum(1, 2))
     }
 }
